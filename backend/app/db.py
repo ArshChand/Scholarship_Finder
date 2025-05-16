2768bea33c249ebe23e886f4894aab30b3c30573
Arsh Chand <arshchand30@gmail.com>
2025-05-16 12:44:34 +0530

    Switch backend to Express

--- backend/app/db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-MONGO_URI = os.getenv("MONGO_URI")
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["scholarship_finder"]

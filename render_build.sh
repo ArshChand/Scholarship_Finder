322873ec717f1ee85dab85d91928b1701ab44dae
Arsh Chand <arshchand30@gmail.com>
2025-05-25 05:55:37 +0530

    Make render_build.sh executable

--- render_build.sh
+++ render_build.sh
@@ -7,7 +7,6 @@ npm install
 # Ensure Puppeteer downloads Chrome to the project-local cache
 npx puppeteer browsers install chrome
 
-# Copy the browser cache to the location Render expects at runtime
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 cp -R .cache/puppeteer/chrome $PUPPETEER_CACHE_DIR/
